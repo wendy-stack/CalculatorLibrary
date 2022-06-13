41c7fb5d5fa2e88f4ce7ce4c52b7ce2987ed3369
wendy <sethwendy@yahoo.com>
2022-06-13 23:36:38 +0100

    modified file

--- calculator.py
+++ calculator.py
@@ -11,5 +11,9 @@ def subtract(first_term, second_term):
     return first_term - second_term
 
 
+def multiply(first_term, second_term):
+    return first_term * second_term
+
+
 def divide(first_term, second_term):
     return first_term / second_term
